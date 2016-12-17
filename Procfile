2d53cc22de203294a390c52fd76fe230e7f37852
Benjamin Legrand <benjilegnard@protonmail.com>
2016-12-17 18:00:41 +0100

    initial files imported from codepen http://codepen.io/jibhaine/pen/3ed87b60ddc3e8a876fc1c03d8a2f8d3

---
+++ Procfile
@@ -0,0 +1,1 @@
+node www/server.min.js
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+var webpack = require('webpack');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path');
+
+module.exports = {
+    entry: {
+       client: "./src/index.ts",
+       // server: "./src/server/index.ts",
+       // test: "./test/index.ts",
+       vendor: ["three","react","react-dom","lodash"]
+    },
+    output: {
+        path: path.join(__dirname, "dist", "www"),
+        filename: "[name].bundle.js"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { 
+                test: /\.tsx?$/,
+                loader: "awesome-typescript",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader?sourceMap", "sass-loader?sourceMap"],
+                exclude: /node_modules/
+            },
+            { 
+                test: /\.pug$/,
+                loader: "pug",
+                exclude: /node_modules/
+            }
+        ],
+
+        preLoaders: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { 
+                test: /\.(js|ts)$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+        "three": "THREE"
+    },
+    plugins: [
+        
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+        new HtmlWebpackPlugin({
+            template: 'src/server/index.pug'
+        }),
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development, 
+            // ./public directory is being served 
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['dist/www'] }
+        })
+    ]
+};
