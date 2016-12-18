ebb5e4e08564b3e007d1efe99f46f07f15b12678
Benjamin Legrand <benjilegnard@protonmail.com>
2016-12-18 15:35:16 +0100

    using webpack.DefinePlugin to pass down NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,9 @@ module.exports = {
         "three": "THREE"
 },*/
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        }),
         new webpack.ProvidePlugin({
             THREE: "three"
         }),
