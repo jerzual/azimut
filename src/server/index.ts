dbd6f4086e1584cddc193fae6d097cc956bbccf8
Benjamin Legrand <jibhaine@free.fr>
2016-12-18 04:15:44 +0100

    added express deps and socket.io-client

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,62 +1,84 @@
 // server entry point
-import * as IO  from "socket.io";
+import * as IO from "socket.io";
 import * as express from "express";
+import methodOverride = require("method-override");
+import bodyParser = require("body-parser");
 import morgan from "morgan";
 import * as winston from "winston";
+import * as cluster from "cluster";
+import { cpus } from 'os';
 
 const serverPort = process.env.PORT || 5000;
-// express app config
-const app:express.Application = express();
-app.use(express.static('./www'));
-const server = app.listen(serverPort, '0.0.0.0', (err)=>{
-    if(err){
-        winston.error('Plague server failed to start', err);
-    }else{
-        winston.info(`Plague server started on ${serverPort}`);
-        winston.info(`environment:`, process.env);
-    }
-});
 
-// socket.io config
-const io = IO();
-io.attach(server);
-io.on("connection", function(socket:SocketIO.Socket){
-    winston.info("Socket connected: ", socket.id);
-    socket.on("action", (action) => {
-        if (action.type === "server/hello") {
-            winston.info("Got hello data!", action.data);
-            socket.emit("action", {type:"message", data:"good day!"});
-        }
+const numCPUs = cpus().length;
+
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        winston.info(`forking workeer #${i}`);
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        winston.warn(`worker ${worker.process.pid} died`);
     });
-});
+} else {
 
-class PlagueRoutes{
-    app:express.Application;
-    constructor(app:express.Application){
-        this.app = app;
-        app.get('/', this.getIndex);
-        app.get('/settings', this.getSettings);
-        // app.get('/game', this.getGame);
-    }
-    getIndex(req: express.Request, res: express.Response, next: express.NextFunction){
-        winston.info('index request');
-    }
-    getSettings(req:express.Request,res: express.Response, next:express.NextFunction) {
-        winston.info('settings request');
-    }
-    putSettings(req:express.Request,res: express.Response, next:express.NextFunction) {
+    // express app config
+    const app: express.Application = express();
+    app.set('port', serverPort);
+    app.use(express.static('./www'));
+    const server = app.listen(
+        app.get('port'),
+         '0.0.0.0', 
+         (err) => {
+        (err) 
+        ? winston.error('Plague server failed to start', err)
+        : winston.info(`Plague server started on ${serverPort}`);
+        
+    });
+
+    // socket.io config
+    const io = IO();
+    io.attach(server);
+    io.on("connection", function (socket: SocketIO.Socket) {
+        winston.info("Socket connected: ", socket.id);
+        socket.on("action", (action) => {
+            if (action.type === "server/hello") {
+                winston.info("Got hello data!", action.data);
+                socket.emit("action", { type: "message", data: "good day!" });
+            }
+        });
+    });
+
+    class PlagueRoutes {
+        app: express.Application;
+        constructor(app: express.Application) {
+            this.app = app;
+            // app.get('/', this.getIndex);
+            // app.get('/settings', this.getSettings);
+            // app.get('/game', this.getGame);
+        }
+        getIndex(req: express.Request, res: express.Response, next: express.NextFunction) {
+            winston.info('index request');
+            res.sendFile('./www/index.html');
+        }
+        getSettings(req: express.Request, res: express.Response, next: express.NextFunction) {
+            winston.info('settings request');
+        }
+        putSettings(req: express.Request, res: express.Response, next: express.NextFunction) {
+
+        }
 
     }
+    // routes:
+    new PlagueRoutes(app);
+    // models
+    //Hero
+    //Party(Session/channel)
+    //Turn
+    //Actions
+    //Scores
+    //Items
+    //City
+    //Tiles
 
 }
-// routes:
-new PlagueRoutes(app) ;
-// models
-//Hero
-//Party(Session/channel)
-//Turn
-//Actions
-//Scores
-//Items
-//City
-//Tiles
