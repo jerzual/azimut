f4086ca26fab031b1e164b422afceeb5b10b4e88
Benjamin Legrand <benjilegnard@protonmail.com>
2016-12-18 14:41:40 +0100

    fixed ressources loading

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,16 +5,19 @@ import methodOverride = require("method-override");
 import bodyParser = require("body-parser");
 import morgan from "morgan";
 import * as winston from "winston";
+import * as path from "path";
 import * as cluster from "cluster";
 import { cpus } from 'os';
+import * as Sequelize from "sequelize";
 
+const env = process.env.NODE_ENV || "development";
 const serverPort = process.env.PORT || 5000;
+const dbUrl = process.env.DATABASE_URL || 'postgres:/localhost:5432/plague';
 
 const numCPUs = cpus().length;
 
 if (cluster.isMaster) {
     for (var i = 0; i < numCPUs; i++) {
-        winston.info(`forking workeer #${i}`);
         cluster.fork();
     }
     cluster.on('exit', (worker, code, signal) => {
@@ -25,16 +28,29 @@ if (cluster.isMaster) {
     // express app config
     const app: express.Application = express();
     app.set('port', serverPort);
-    app.use(express.static('./www'));
+    app.use(express.static(path.join(__dirname,'www'));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
+    // sequelize config
     const server = app.listen(
         app.get('port'),
-         '0.0.0.0', 
-         (err) => {
-        (err) 
-        ? winston.error('Plague server failed to start', err)
-        : winston.info(`Plague server started on ${serverPort}`);
-        
-    });
+        '0.0.0.0',
+        (err) => {
+            (err)
+                ? winston.error('Plague server failed to start', err)
+                : winston.info(`Plague server started on ${serverPort}`);
+
+        });
+
+    const db = new Sequelize(dbUrl);
+    db.sync().then(
+        (result) => {
+            winston.debug('Sequelize initialized', result);
+        }).catch(
+        (err) => {
+            winston.error('Sequelize error : ', err);
+        });
 
     // socket.io config
     const io = IO();
@@ -53,13 +69,13 @@ if (cluster.isMaster) {
         app: express.Application;
         constructor(app: express.Application) {
             this.app = app;
-            // app.get('/', this.getIndex);
+            app.get('/', this.getIndex);
             // app.get('/settings', this.getSettings);
             // app.get('/game', this.getGame);
         }
         getIndex(req: express.Request, res: express.Response, next: express.NextFunction) {
             winston.info('index request');
-            res.sendFile('./www/index.html');
+            res.sendFile(path.join(__dirname, 'www','index.html'));
         }
         getSettings(req: express.Request, res: express.Response, next: express.NextFunction) {
             winston.info('settings request');
@@ -80,5 +96,4 @@ if (cluster.isMaster) {
     //Items
     //City
     //Tiles
-
 }
