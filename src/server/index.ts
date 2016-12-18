c8378708bf466d86e7fa99430e8b33f1603c65c9
Benjamin Legrand <jibhaine@free.fr>
2016-12-18 01:24:07 +0100

    use winston logging on the server and fixed app.listen wrong callback

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,23 +2,29 @@
 import * as IO  from "socket.io";
 import * as express from "express";
 import morgan from "morgan";
+import * as winston from "winston";
 
 const serverPort = process.env.PORT || 5000;
 // express app config
 const app:express.Application = express();
 app.use(express.static('./www'));
-const server = app.listen(serverPort,()=>{
-    console.log(`Plague server started on ${serverPort}`);
+const server = app.listen(serverPort, '0.0.0.0', (err)=>{
+    if(err){
+        winston.error('Plague server failed to start', err);
+    }else{
+        winston.info(`Plague server started on ${serverPort}`);
+        winston.info(`environment:`, process.env);
+    }
 });
 
 // socket.io config
 const io = IO();
 io.attach(server);
 io.on("connection", function(socket:SocketIO.Socket){
-    console.log("Socket connected: " + socket.id);
+    winston.info("Socket connected: ", socket.id);
     socket.on("action", (action) => {
         if (action.type === "server/hello") {
-            console.log("Got hello data!", action.data);
+            winston.info("Got hello data!", action.data);
             socket.emit("action", {type:"message", data:"good day!"});
         }
     });
@@ -33,10 +39,10 @@ class PlagueRoutes{
         // app.get('/game', this.getGame);
     }
     getIndex(req: express.Request, res: express.Response, next: express.NextFunction){
-        console.log('index request');
+        winston.info('index request');
     }
     getSettings(req:express.Request,res: express.Response, next:express.NextFunction) {
-        console.log('settings request');
+        winston.info('settings request');
     }
     putSettings(req:express.Request,res: express.Response, next:express.NextFunction) {
 
