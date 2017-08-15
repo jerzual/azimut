fcd24ddd88186d716a160730bab91112035cf7f2
Benjamin Legrand <jibhaine@free.fr>
2017-08-15 12:55:41 +0200

    fix(build-client) : adds an es6 to es5 transpilation step with babel (for polyfills etc.).

--- webpack.config.js
+++ webpack.config.js
@@ -75,11 +75,11 @@ const config =  {
 
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            // Typescript compiles to es6 then babel transpiles it to es5.
             {
                 test: /\.tsx?$/,
-                use:  [ { loader: "awesome-typescript-loader" } ],
-                exclude: /node_modules|server/
+                use:  [ "babel-loader", "awesome-typescript-loader" ],
+                exclude: [/node_modules/, /\/server/, /__test__/ ]
             },
             {
                 test: /\.scss$/,
@@ -92,26 +92,19 @@ const config =  {
                 exclude: /node_modules/
             }
         ],
-/*
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                test: /\.(js|ts)$/,
-                loader: "source-map-loader"
-            }
-        ]
-*/
     },
 
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    /*externals: {
+    externals: {
         "react": "React",
         "react-dom": "ReactDOM",
-        "three": "THREE"
-},*/
+        "babylonjs": "BABYLON",
+        "tone": "Tone",
+        "rot.js": "ROT"
+    },
     plugins: plugins
 };
 
