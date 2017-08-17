0ed2dd8a5b83b661c622df565ea90bee65595727
Benjamin Legrand <jibhaine@free.fr>
2017-08-17 02:06:39 +0200

    feat(lights) : DayNightcycle gives an angle from a moment() object.

--- src/views/render/DayNightCycle.ts
+++ src/views/render/DayNightCycle.ts
@@ -1,4 +1,5 @@
 import { DirectionalLight } from "babylonjs";
+import * as moment from 'moment';
 
 const HOUR_PER_DAY = 24;
 const MIN_PER_HOUR = 60;
@@ -8,26 +9,42 @@ export enum Phase {
     night, dawn, day, dusk
 }
 export class DayNightCycle {
-    timeOfDay: number;
+    // input
+    timeOfDay: moment.Moment;
+    // output angle on 0/2*PI scale
     timeAngle: number;
+    // move current angle
     speed: number;
     phase: Phase;
-    sun: DirectionalLight;
+    minutesOfDay:number;
     constructor() {
-        this.timeAngle = TAU / 2;
+        this.timeAngle = 0;
         this.phase = Phase.night;
     }
-    tick() {
+    tick(minutes:number) {
+        this.timeOfDay = this.timeOfDay.add(minutes, 'minute');
+    }
+    update(update:moment.Moment) {
+        this.timeOfDay = update;
+        // Your moment at midnight
+        var mmtMidnight:moment.Moment = moment().clone().startOf('day');
+
+        // Difference in minutes
+        this.minutesOfDay = update.diff(mmtMidnight, 'minutes', true);
+
+        this.timeAngle = (this.minutesOfDay / (MIN_PER_HOUR * HOUR_PER_DAY)) * TAU;
+        // fois un tours d'horloge
+        return this.timeAngle;
+    }
+    private setTimeAngle(){
+        
         // go back 360° if we
         if (this.timeAngle > TAU) {
             this.timeAngle += this.speed;
         }
     }
-    get hour(){
-        return this.timeAngle / TAU * HOUR_PER_DAY;
-    }
-    get min(){
-        return this.timeAngle / TAU * HOUR_PER_DAY * MIN_PER_HOUR;
+    private setPhase(){
+        
     }
 }
 
