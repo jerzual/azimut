55293b0a1bdde42aad99f3ce1929cf9ef5c82ee8
Benjamin Legrand <jibhaine@free.fr>
2019-10-09 22:59:58 +0200

    feat(nestjs): add universal/nestjs server

---
+++ server/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { ApplicationModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ApplicationModule);
+  app.enableCors({
+    methods: 'GET',
+    maxAge: 3600,
+  });
+  await app.listen(4000);
+}
+bootstrap().catch(err => console.error(err));
