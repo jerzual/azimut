dd3233257133e63e6b52c7146f39dcf2a853b6e4
jibhaine <jibhaine@free.fr>
2020-05-20 19:32:47 +0200

    ng update @nguniversal/express-engine

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,7 +3,6 @@ import {
   ServerModule,
   ServerTransferStateModule,
 } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -18,9 +17,8 @@ import { WindowService } from './core/services/window.service';
     // by the ServerModule from @angular/platform-server.
     AppModule,
     ServerModule,
-    ServerTransferStateModule,
-    ModuleMapLoaderModule, // <-- *Important* to have lazy-loaded routes work
-  ],
+    ServerTransferStateModule
+],
   // Since the bootstrapped component is not inherited from your
   // imported AppModule, it needs to be repeated here.
   bootstrap: [AppComponent],
--- src/main.server.ts
+++ src/main.server.ts
@@ -6,7 +6,6 @@ if (environment.production) {
 }
 
 export { ngExpressEngine } from '@nguniversal/express-engine';
-export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 export { AppServerModule } from './app/app.server.module';
 
 export { renderModule, renderModuleFactory } from '@angular/platform-server';
