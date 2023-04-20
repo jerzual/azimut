39c3fe8419ea70ac3c50e74d0086ca07a29dc48b
Benjamin Legrand <benjilegnard@protonmail.com>
2023-04-20 13:22:44 +0200

    chore(standalone): Bootstrap the project using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoadingComponent } from './core/components/loading/loading.component';
+import { NgIf } from '@angular/common';
+import { SceneComponent } from './scene/components/scene/scene.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
+	standalone: true,
+	imports: [NgIf, LoadingComponent, RouterOutlet, SceneComponent],
 })
 export class AppComponent implements OnInit {
 	public loading = true;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,68 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { getWindow } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {
+	StoreRouterConnectingModule,
+	FullRouterStateSerializer,
+} from '@ngrx/router-store';
+import { AppEffects } from './app/app.effects';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { CoreModule } from './app/core/core.module';
+import {
+	withInterceptorsFromDi,
+	provideHttpClient,
+} from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { WindowService } from './app/core/services/window.service';
+import { REDUCERS_TOKEN, reducers, metaReducers } from './app/reducers';
 
 if (environment.production) {
 	enableProdMode();
 }
 
-platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+	providers: [
+		importProvidersFrom(
+			BrowserModule.withServerTransition({ appId: 'azimut' }),
+			TransferHttpCacheModule,
+			AppRoutingModule,
+			CoreModule,
+			// ngrx
+			StoreModule.forRoot(REDUCERS_TOKEN, {
+				metaReducers,
+				runtimeChecks: {
+					strictStateImmutability: true,
+					strictActionImmutability: true,
+				},
+			}),
+			EffectsModule.forRoot([AppEffects]),
+			StoreRouterConnectingModule.forRoot({
+				serializer: FullRouterStateSerializer,
+			}),
+			StoreDevtoolsModule.instrument({
+				maxAge: 25,
+				logOnly: environment.production,
+			}),
+		),
+		// FIXME: this do not work anymore on ssr with angular 14 upgrade.
+		// {
+		//   provide: APP_INITIALIZER,
+		//   useFactory: initApplication,
+		//   deps: [Store],
+		//   multi: true,
+		// },
+		{ provide: REDUCERS_TOKEN, useValue: reducers },
+		{
+			provide: WindowService,
+			useFactory: getWindow,
+		},
+		provideHttpClient(withInterceptorsFromDi()),
+	],
+}).catch((err) => console.error(err));
