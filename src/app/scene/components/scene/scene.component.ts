29333bf141d74a84f756c1a924b3aadc3aa9bec4
Benjamin Legrand <benjilegnard@protonmail.com>
2024-09-07 17:47:44 +0200

    feat: add and use angular-three v2

--- src/app/scene/components/scene/scene.component.ts
+++ src/app/scene/components/scene/scene.component.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy } from '@angular/core';
+import { extend, NgtCanvas } from 'angular-three';
+import { Mesh, BoxGeometry } from 'three';
+
+
+extend({
+	Mesh, // makes ngt-mesh available
+	BoxGeometry, // makes ngt-box-geometry available
+});
+
+
+
+@Component({
+	// This Component is rendered in the Custom Renderer
+	standalone: true,
+	template: `
+		<ngt-mesh>
+			<ngt-box-geometry />
+		</ngt-mesh>
+	`,
+	schemas: [CUSTOM_ELEMENTS_SCHEMA], // required
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SceneGraphComponent {}
 
 @Component({
 	selector: 'app-scene',
@@ -16,11 +39,9 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 			}
 		`,
 	],
-	template: `<canvas id="renderZone" #renderZone></canvas>`,
+	imports: [NgtCanvas],
+	template: `<ngt-canvas [sceneGraph]="SceneGraph"></ngt-canvas>`,
 })
 export class SceneComponent {
-	@ViewChild('renderZone', { static: true })
-	public renderZone: ElementRef<HTMLCanvasElement>;
-
-	constructor() {}
+	SceneGraph = SceneGraphComponent;
 }
